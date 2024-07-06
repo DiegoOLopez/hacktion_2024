365404dfce00f3ebf3803a163bb9f65f50c24b9b
AlanHerCas <yosky.425@gmail.com>
2024-07-06 16:46:06 -0600

    Transcripcion y envio de post de la transcripcion, descarga de la transcripcion

--- front/src/app/funciones/dictado/dictado.component.ts
+++ front/src/app/funciones/dictado/dictado.component.ts
@@ -1,13 +1,17 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, HostListener } from '@angular/core';
+import { TranscripcionService } from '../transcripcion.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
-  selector: 'app-dictado',
+  selector: 'app-dictation',
   templateUrl: './dictado.component.html',
   styleUrls: ['./dictado.component.css']
 })
 export class DictationComponent {
   recognition: any;
   transcript: string = '';
+  isRecording: boolean = false;
 
   constructor(private zone: NgZone) {
     const { webkitSpeechRecognition }: IWindow = window as any;
@@ -15,7 +19,6 @@ export class DictationComponent {
     this.recognition.continuous = true;
     this.recognition.interimResults = false;
     this.recognition.lang = 'es-ES';
-    this.recognition.lang = 'en-EN'
 
     this.recognition.onresult = (event: any) => {
       const transcriptResult = event.results[event.results.length - 1][0].transcript;
@@ -29,16 +32,60 @@ export class DictationComponent {
     };
   }
 
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.code === 'Space' && !this.isRecording) {
+      this.startDictation();
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  handleKeyUp(event: KeyboardEvent) {
+    if (event.code === 'Space' && this.isRecording) {
+      this.stopDictation();
+    }
+  }
+
   startDictation() {
     this.transcript = '';
+    this.isRecording = true;
     this.recognition.start();
   }
 
   stopDictation() {
-    this.recognition.stop();
+    setTimeout(() => {
+      this.recognition.stop();
+      this.isRecording = false;
+    }, 2000); // 5000 milisegundos = 5 segundos
   }
+
+  downloadTranscript() {
+    const blob = new Blob([this.transcript], { type: 'text/plain;charset=utf-8' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'transcripcion.txt';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }
+
+  // saveTranscript() {
+  //   this.TranscripcionService.saveTranscription(this.transcript).subscribe(
+  //     response => {
+  //       console.log('Transcripción guardada con éxito:', response);
+  //     },
+  //     error => {
+  //       console.error('Error al guardar la transcripción:', error);
+  //     }
+  //   );
+  // }
+
 }
 
 interface IWindow extends Window {
   webkitSpeechRecognition: any;
 }
+
+module exports {DictationComponent};
