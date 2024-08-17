7b8fed5e593e1e4f6844fb79b3d10f8c297411d1
Yael UwU <pyael.soto@gmail.com>
2024-08-17 14:47:51 -0600

    se comento el codigo de la sentencia

--- back/sentencia.js
+++ back/sentencia.js
@@ -1,23 +1,20 @@
 const express = require('express');
-//const { Chat } = require('openai/src/resources/index.js');
 const app = express();
 const port = 3000;
+// se agrega la libreria de geminis
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+//TODO se tiene que agregar esto en una variable de entorno
+//aqui se agrega el token
 const genAI = new GoogleGenerativeAI("AIzaSyDbG0KxTyTKLbuTNfkyGnGsr6Ro4Tcdlcg");
+//se selecciona el tipo de modelo
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
-// const prompt = "Write a story about a magic backpack.";
-
-// const result = await model.generateContent(prompt);
-// console.log(result.response.text());
-
+//se llama a la IA con la sentencia que se recupera del front
 async function recopilacion_de_sentencia(sentencia) {
-  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
   const prompt = "De la siguiente sentencia: "+ sentencia + ", regresame el siguiente formato, y solo ponlo entre corchetes, no quiero que digas nadax: ['Down (primer, segundo, tercero, cuarto', 'tipo de jugada (pase, carrera)', 'yardas (positivas o negativas y el numero)']"
-
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const texto = response.text();
