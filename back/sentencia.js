b0e0b85cd87cd4e3769fb2a816645537aceafe84
Yael Soto <pyael.soto@gmail.com>
2024-08-23 00:43:56 -0600

    se agrego variable de entorno y prueba

--- back/sentencia.js
+++ back/sentencia.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+const apikey=process.env.API_KEY_GEMINIS;
 // se agrega la libreria de geminis
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 //TODO se tiene que agregar esto en una variable de entorno
 //aqui se agrega el token
-const genAI = new GoogleGenerativeAI("AIzaSyDpC1qFGxMlSzlWC6P-3eoqpkQsFmmJfoE");
+const genAI = new GoogleGenerativeAI(apikey);
 //se selecciona el tipo de modelo
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
@@ -14,7 +16,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 async function recopilacion_de_sentencia(sentencia) {
 
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-  const prompt = "De la siguiente sentencia: "+ sentencia + ", regresame el siguiente formato, y solo ponlo entre corchetes, no quiero que digas nada: ['Down (primer, segundo, tercero, cuarto', 'tipo de jugada (pase, carrera)', 'yardas (positivas o negativas y el numero)']"
+  const prompt = "De la siguiente sentencia: "+ sentencia + "regresame el siguiente formato, y solo ponlo entre corchetes, no quiero que digas nada: ['Down (primer, segundo, tercero, cuarto', 'numero de jugador ', 'hasta que yarda llego', 'tipo de jugada (pase, carrera)', 'yardas (positivas o negativas y el numero)', 'equipo 1', 'equipo 2']"
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const texto = response.text();
